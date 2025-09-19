0221d0bbee56008dc2219435a7b620a41490057b
ZY <ZYfrance@proton.me>
2025-09-19 10:14:31 +0800

    919_1

--- scripts/AMPTST/main/doall.sh
+++ scripts/AMPTST/main/doall.sh
@@ -1,9 +1,9 @@
-bash .\\scripts\\AMPTST\\main\\AMPTSTps1_10.sh
-bash .\\scripts\\AMPTST\\main\\AMPTST.sh
-bash .\scripts\AMPTST\sota\runable\PSLOSS\TimesNet.sh
-bash .\scripts\AMPTST\sota\runable\PSLOSS\iTransformer.sh
-bash .\scripts\AMPTST\sota\runable\PSLOSS\PatchTSTv1.sh
-bash .\scripts\AMPTST\sota\runable\PSLOSS\DLinear.sh
-bash .\scripts\AMPTST\sota\runable\PSLOSS\TimeMixer.sh
+#bash .\\scripts\\AMPTST\\main\\AMPTSTps1_10.sh
+#bash .\\scripts\\AMPTST\\main\\AMPTST.sh
+bash .\\scripts\\AMPTST\\sota\\runable\\PSLOSS\\TimesNet.sh
+bash .\\scripts\\AMPTST\\sota\\runable\\PSLOSS\\iTransformer.sh
+bash .\\scripts\\AMPTST\\sota\\runable\\PSLOSS\\PatchTSTv1.sh
+bash .\\scripts\\AMPTST\\sota\\runable\\PSLOSS\\DLinear.sh
+bash .\\scripts\\AMPTST\\sota\\runable\\PSLOSS\\TimeMixer.sh
 
 
