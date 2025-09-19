05b8a283a5d72f44c0a5ee68a480f9c2872849ba
ZY <ZYfrance@proton.me>
2025-09-19 17:11:43 +0800

    919_3

---
+++ scripts/AMPTST/main/psweight10.sh
@@ -0,0 +1,68 @@
+export CUDA_VISIBLE_DEVICES=0
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/AMPTST/main" ]; then
+    mkdir ./logs/AMPTST/main
+fi
+model_name=AMPTST
+
+ps_lambdas=(10.0)
+
+seq_len=168
+e_layers=3
+down_sampling_layers=3
+down_sampling_window=2
+learning_rate=0.01
+d_model=32
+d_ff=32
+train_epochs=20
+patience=10
+f=57
+data_path=h57.csv
+des=CM
+for pred_len in 24 168
+do
+  for ps_lambda in ${ps_lambdas[@]}
+  do
+    python -u run.py \
+      --task_name long_term_forecast \
+      --is_training 1 \
+      --root_path ./dataset/mydata_v1/ \
+      --data_path $data_path \
+      --model_id h57 \
+      --model $model_name \
+      --data custom \
+      --features MS \
+      --seq_len $seq_len \
+      --label_len 0 \
+      --pred_len $pred_len \
+      --n_heads 8 \
+      --e_layers $e_layers \
+      --d_layers 1 \
+      --factor 3 \
+      --top_k 3 \
+      --enc_in $f \
+      --dec_in $f \
+      --c_out $f \
+      --des 10_1 \
+      --itr 1 \
+      --d_model $d_model \
+      --d_ff $d_ff \
+      --moving_avg 25 \
+      --batch_size 16 \
+      --learning_rate $learning_rate \
+      --train_epochs $train_epochs \
+      --patience $patience \
+      --channel_independence 0 \
+      --down_sampling_layers $down_sampling_layers \
+      --down_sampling_method avg \
+      --down_sampling_window $down_sampling_window \
+      --pf 0 \
+      --ps_lambda $ps_lambda \
+      --use_ps_loss 1 \
+      --head_or_projection 1 \
+      --result_path psweight
+    done
+done
