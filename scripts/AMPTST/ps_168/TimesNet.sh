7d44510d27dbb38fcd5543c2d762ed028a4bedc7
ZY <ZYfrance@proton.me>
2025-09-24 19:22:30 +0800

    Local924

--- scripts/AMPTST/ps_168/TimesNet.sh
+++ scripts/AMPTST/ps_168/TimesNet.sh
@@ -1,9 +1,10 @@
+
 export CUDA_VISIBLE_DEVICES=0
 model_name=TimesNet
 ps_lambdas=(2.0 6.0 10.0)
 f=57
 seq_len=168
-for pred_len in 24 48 96 168
+for pred_len in 168
 do
   for ps_lambda in ${ps_lambdas[@]}
   do
